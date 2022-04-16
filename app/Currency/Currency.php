fd01941cbcb567434285cb3f7e5b4cbda016f1ae
Marcus Jian <renyou815@gmail.com>
2022-04-16 14:52:34 +0800

    feat: implement currency conversion

--- app/Currency/Currency.php
+++ app/Currency/Currency.php
@@ -6,8 +6,41 @@ class Currency
 {
     protected array $forexRate;
 
+    protected string $amount;
+
+    protected string $from;
+
+    protected string $to;
+
     public function __construct(array $forexRate)
     {
         $this->forexRate = $forexRate;
+    }
+
+    public function get(): string
+    {
+        return bcmul(
+            $this->amount,
+            $this->forexRate[$this->from][$this->to],
+            2
+        );
+    }
+
+    public function amount(string $amount): static
+    {
+        $this->amount = $amount;
+        return $this;
+    }
+
+    public function from(string $from): static
+    {
+        $this->from = $from;
+        return $this;
+    }
+
+    public function to(string $to): static
+    {
+        $this->to = $to;
+        return $this;
     }
 }
